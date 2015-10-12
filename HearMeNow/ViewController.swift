52e1280b8c0d3929bd8aa35c7f814ec430981c91
ivan lares <ivanlares1@gmail.com>
2015-10-12 00:25:30 -0700

    Added .gitignore file, updated to Swift 2.0

--- HearMeNow/ViewController.swift
+++ HearMeNow/ViewController.swift
@@ -10,126 +10,93 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDelegate {
+  
+  var hasRecording = false
+  var soundPlayer: AVAudioPlayer?
+  var soundRecorder: AVAudioRecorder?
+  var session: AVAudioSession?
+  var soundPath: String?
+  
+  @IBOutlet weak var recordButton: UIButton!
+  @IBOutlet weak var playButton: UIButton!
+  @IBAction func recordPressed(sender: AnyObject) {
     
-    var hasRecording = false
-    var soundPlayer: AVAudioPlayer?
-    var soundRecorder: AVAudioRecorder?
-    var session: AVAudioSession?
-    var soundPath: String?
-    
-    
-    
-    @IBOutlet weak var recordButton: UIButton!
-
-    @IBOutlet weak var playButton: UIButton!
-    
-    @IBAction func recordPressed(sender: AnyObject) {
-        
-        if(soundRecorder?.recording == true){
-            soundRecorder?.stop()
-            recordButton.setTitle("Record", forState: UIControlState.Normal)
-            hasRecording = true
+    if(soundRecorder?.recording == true){
+      soundRecorder?.stop()
+      recordButton.setTitle("Record", forState: UIControlState.Normal)
+      hasRecording = true
+    } else {
+      session?.requestRecordPermission(){
+        granted in
+        if (granted == true){
+          self.soundRecorder?.record()
+          self.recordButton.setTitle("Stop", forState: UIControlState.Normal)
         } else {
-            session?.requestRecordPermission(){
-                granted in
-                if (granted == true){
-                    self.soundRecorder?.record()
-                    self.recordButton.setTitle("Stop", forState: UIControlState.Normal)
-                } else {
-                    println("Unable to record")
-                }
-            }
+          print("Unable to record")
         }
-
+      }
     }
-    
-    @IBAction func playPressed(sender: AnyObject) {
-        if (soundPlayer?.playing == true){
-            soundPlayer?.pause()
-            playButton.setTitle("Play", forState: UIControlState.Normal)
-        } else if (hasRecording == true){
-            let url = NSURL(fileURLWithPath: soundPath!)
-            var error: NSError?
-            soundPlayer = AVAudioPlayer(contentsOfURL: url, error: &error)
-            
-            if (error == nil){
-                soundPlayer?.delegate = self
-                soundPlayer?.enableRate = true
-                soundPlayer?.rate = 0.5
-                soundPlayer?.play()
-            }else {
-                println("Error initializing player \(error)")
-            }
-            playButton.setTitle("Pause", forState: UIControlState.Normal)
-            hasRecording = false
-        } else if (soundPlayer != nil) {
-            soundPlayer?.play()
-            playButton.setTitle("Pause", forState: UIControlState.Normal)
-        }
+  }
+  
+  @IBAction func playPressed(sender: AnyObject) {
+    if (soundPlayer?.playing == true){
+      soundPlayer?.pause()
+      playButton.setTitle("Play", forState: UIControlState.Normal)
+    } else if (hasRecording == true){
+      let url = NSURL(fileURLWithPath: soundPath!)
+      do {
+        soundPlayer = try AVAudioPlayer(contentsOfURL: url)
+        soundPlayer?.delegate = self
+        soundPlayer?.enableRate = true
+        soundPlayer?.rate = 1.0
+        soundPlayer?.play()
+      } catch let error as NSError {
+        print(error.localizedDescription)
+        soundPlayer = nil
+      }
+      playButton.setTitle("Pause", forState: UIControlState.Normal)
+      hasRecording = false
+    } else if (soundPlayer != nil){
+      soundPlayer?.play()
+      playButton.setTitle("Pause", forState: UIControlState.Normal)
     }
-    
-    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder!, successfully flag: Bool) {
-        recordButton.setTitle("Record", forState: UIControlState.Normal)
+  }
+  
+  func audioRecorderDidFinishRecording(recorder: AVAudioRecorder, successfully flag: Bool) {
+    recordButton.setTitle("Record", forState: UIControlState.Normal)
+  }
+  
+  func audioPlayerDidFinishPlaying(player: AVAudioPlayer, successfully flag: Bool) {
+    playButton.setTitle("Play", forState: UIControlState.Normal)
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    soundPath = "\(NSTemporaryDirectory())hearmenow.wav"
+    let url = NSURL(fileURLWithPath:soundPath!)
+    session = AVAudioSession.sharedInstance()
+    do {
+      try session?.setActive(true)
+      try session?.setCategory(AVAudioSessionCategoryPlayAndRecord)
+    } catch let error as NSError {
+      print(error.localizedDescription)
     }
-    func audioPlayerDidFinishPlaying(player: AVAudioPlayer!, successfully flag: Bool) {
-        playButton.setTitle("Play", forState: UIControlState.Normal)
+    do {
+      try session?.setCategory(AVAudioSessionCategoryPlayAndRecord)
+    } catch let error as NSError {
+      print(error.localizedDescription)
     }
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        soundPath = "\(NSTemporaryDirectory())hearmenow.wav"
-        let url = NSURL(fileURLWithPath: soundPath!)
-        
-        session = AVAudioSession.sharedInstance()
-        session?.setActive(true, error: nil)
-        var error: NSError?
-        session?.setCategory(AVAudioSessionCategoryPlayAndRecord, error: &error)
-        
-        soundRecorder = AVAudioRecorder(URL: url, settings: nil, error: &error)
-        if (error != nil){
-            println("Error initializing the recorder: \(error)")
-        }
-        soundRecorder?.delegate = self
-        soundRecorder?.prepareToRecord()
-        
+    do {
+      let dictionary = [String:AnyObject]()
+      soundRecorder = try AVAudioRecorder(URL: url, settings: dictionary)
+    } catch let error as NSError {
+      print(error.localizedDescription)
+      soundRecorder = nil
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-
+    soundRecorder?.delegate = self
+    soundRecorder?.prepareToRecord()
+  }
+  
 }
 
