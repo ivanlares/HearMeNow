aaf11ca45705ab807e0d7bfb87945d4251ef90ea
ivan lares <ivanlares1@gmail.com>
2015-10-13 10:23:14 -0700

    Moved .gitignore file

--- HearMeNowTests/HearMeNowTests.swift
+++ HearMeNowTests/HearMeNowTests.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 import XCTest
+@testable import HearMeNow
 
 class HearMeNowTests: XCTestCase {
-    
+  
+  
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
